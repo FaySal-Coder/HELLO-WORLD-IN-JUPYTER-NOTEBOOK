747abe8608d519bc2129ea96391f249124b1ca23
FaySal-Coder <mfaisalatari@gmail.com>
2024-09-02 03:34:19 -0700

    code changes

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-name:str = "faisal" 
+name:str = "Faisal" 
 print (name)
 
-print (id(name))
+#print (id(name))
 
-print (dir(name))
+#print (dir(name))
 
 
+print (name.capitalize())
+
+print (name.casefold())
+print (name.center(10,"*"))
+
+print([i for i in dir (name) if not i.startswith("_")])
